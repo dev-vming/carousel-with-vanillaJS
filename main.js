de4de4f76b646111d019f95d3127fe034415826b
dev-vming <dev.vming@gmail.com>
2025-01-16 13:51:02 +0900

    Feat: 슬라이드 기능 추가

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const iconNext = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+</svg>
+`;
+const iconPrev = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+</svg>
+`;
+
 const app = createElement({
   tagName: "div",
   parent: document.body,
@@ -19,75 +28,115 @@ width: 100%;
 height: 100%;
 position: relative;
 display: flex;
-`;
-
-const iconNext = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-</svg>
-`;
-const iconPrev = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path color="white" stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-</svg>
-`;
-
-const [prevBtn, nextBtn] = createElement({
-  tagName: "button",
-  parent: wrapper,
-  count: 2,
-});
-
-prevBtn.style.cssText = `
-position: absolute;
-z-index: 1;
-border: 0;
-top: 0;
-width: 50px;
-height: 100%;
-background-color: transparent;
-background: linear-gradient(90deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.1) 30%, rgba(0,0,0,0) 100%);
-filter: drop-shadow(3px 5px 2px rgb(0 0 0 / 0.7));
-`;
-
-nextBtn.style.cssText = prevBtn.style.cssText;
-nextBtn.style.background =
-  "linear-gradient(270deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.1) 30%, rgba(0,0,0,0) 100%)";
-prevBtn.style.left = "0px";
-nextBtn.style.right = "0px";
-
-prevBtn.innerHTML = iconPrev;
-nextBtn.innerHTML = iconNext;
-
-const container = createElement({
-  tagName: 'div',
-  parent: wrapper,
-});
-container.style.cssText = `
-width: 700px;
-height: 250px;
-display: flex;
-justify-content: center;
-align-items: center;
 overflow: hidden;
 `;
 
-const image = createElement({
-  tagName: 'img',
-  parent: container,
-  properties: { src: './images/01.jpg' }
+addBtn();
+
+const itemContainer = createElement({
+  tagName: 'div',
+  parent: wrapper,
 });
 
-const caption = createElement({
-  tagName: 'span',
-  properties: { innerText: 'Caption text' },
-  parent: container,
-});
-
-caption.style.cssText = `
-color: white;
-font-weight: bold;
-position: absolute;
-filter: drop-shadow(3px 3px 3px rgb(0 0 0 / 0.5));
+itemContainer.style.cssText = `
+display: flex;
+transform: translateX(-700px)
 `;
+
+addImageItem(itemContainer, './images/01.jpg');
+addImageItem(itemContainer, './images/02.jpg');
+addImageItem(itemContainer, './images/03.jpg');
+addImageItem(itemContainer, './images/04.jpg');
+
+function addBtn() {
+  const [prevBtn, nextBtn] = createElement({
+    tagName: "button",
+    parent: wrapper,
+    count: 2,
+  });
+  
+  prevBtn.style.cssText = `
+  position: absolute;
+  z-index: 1;
+  border: 0;
+  top: 0;
+  width: 50px;
+  height: 100%;
+  background-color: transparent;
+  background: linear-gradient(90deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.1) 30%, rgba(0,0,0,0) 100%);
+  filter: drop-shadow(3px 5px 2px rgb(0 0 0 / 0.7));
+  `;
+  
+  nextBtn.style.cssText = prevBtn.style.cssText;
+  nextBtn.style.background =
+    "linear-gradient(270deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.1) 30%, rgba(0,0,0,0) 100%)";
+  prevBtn.style.left = "0px";
+  nextBtn.style.right = "0px";
+  
+  prevBtn.innerHTML = iconPrev;
+  nextBtn.innerHTML = iconNext;
+  
+  nextBtn.onclick = () => {
+    handleSlide();
+  }
+  
+  prevBtn.onclick = () => {
+    handleSlide(false);
+  }
+};
+
+function handleSlide(next = true) {
+  itemContainer.style.transitionDuration = '0.5s';
+  itemContainer.style.transform = `translateX(${next ? -1400 : 0}px)`;
+
+  itemContainer.ontransitionend = () => {
+    itemContainer.style.removeProperty('transition-duration');
+    itemContainer.style.transform = 'translateX(-700px)';
+
+    next
+      ? itemContainer.appendChild(itemContainer.firstChild)
+      :itemContainer.prepend(itemContainer.lastChild);
+  }
+};
+
+function addImageItem(parent, src, captionText = 'Caption text') {
+  const container = createElement({
+    tagName: 'div',
+    parent,
+  });
+  container.style.cssText = `
+  width: 700px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  background-color: #000;
+  position: relative;
+  `;
+  
+  const image = createElement({
+    tagName: 'img',
+    parent: container,
+    properties: { src }
+  });
+  
+  const caption = createElement({
+    tagName: 'span',
+    properties: { innerText: captionText },
+    parent: container,
+  });
+  
+  caption.style.cssText = `
+  color: white;
+  font-weight: bold;
+  position: absolute;
+  filter: drop-shadow(3px 3px 3px rgb(0 0 0 / 0.5));
+  `;
+
+  return container;
+}
+
 
 function createElement({ tagName, properties, parent, children, count = 1 }) {
   const create = () => {
